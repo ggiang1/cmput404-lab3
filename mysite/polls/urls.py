d5def9a083888754cd9f5d34061a446bd29b9fa2
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2023-10-04 16:54:08 -0600

    Edits

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 app_name = "polls"
-urlpatterns = [
+urlpatterns = [ # URL Patterns, can be used in the HTMLs
   path('', views.IndexView.as_view(), name='index'),
   path('<int:pk>/', views.DetailView.as_view(), name='detail'), #pk => primary key
   # ex: /polls/5/results/
-  path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+  path('<int:pk>/results/', views.ResultsView.as_view(), name='results'), # Change int to str if we want strings
   # ex: /polls/5/vote/
   path('<int:question_id>/vote/', views.vote, name='vote'),
   path('api/questions/', views.get_questions, name='get_questions'),
